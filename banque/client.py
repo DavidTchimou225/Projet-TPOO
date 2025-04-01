82a754bf698256c7aa6a1d45486e8603fb8fde82
DavidTchimou225 <tchimoudavid13@gmail.com>
2025-04-01 11:50:58 +0000

    Create client.py
    Table client de la banque

---
+++ banque/client.py
@@ -0,0 +1,24 @@
+class ClientUtilisateur:
+    def __init__(self, nom, adresse, telephone, cnic, login, mot_de_passe):
+        self.nom = nom
+        self.adresse = adresse
+        self.telephone = telephone
+        self.cnic = cnic
+        self.login = login
+        self.mot_de_passe = mot_de_passe
+        self.comptes = []
+
+    def ajouter_compte(self, compte):
+        self.comptes.append(compte)
+
+class ClientEntreprise:
+    def __init__(self, nom_entreprise, adresse, numero_fiscal, login, mot_de_passe):
+        self.nom_entreprise = nom_entreprise
+        self.adresse = adresse
+        self.numero_fiscal = numero_fiscal
+        self.login = login
+        self.mot_de_passe = mot_de_passe
+        self.comptes = []
+
+    def ajouter_compte(self, compte):
+        self.comptes.append(compte)
